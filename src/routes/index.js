e7c327228b071990ccbbbd036dc6e452b311ffbb
Sergio Rosa <scarlosjazz@gmail.com>
2019-08-15 21:09:47 -0400

    Private routes  with React-router-dom

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import  Route from './Route';
 
-import '../config/ReactotronConfig';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
-import Routes from '../routes';
-import history from '../services/history';
 
-
- function App() {
+export default function Routes() {
   return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register"  component={SignUp} />
+      <Route path="/dashboard"  component={Dashboard} isPrivate />
+      <Route path="/profile"  component={Profile} isPrivate />
 
-      <Router history={history}>
-        <Routes />
-      </Router>
-
+      {/* <Route path="/" component={() => <h1>404</h1>} /> */}
+    </Switch>
   )
 }
-export default App;

c68a60b4a6095c2531eb47791d1b2930777f55a6
Sergio Rosa <scarlosjazz@gmail.com>
2019-08-16 19:02:45 -0400

    SetUp Authentication login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return<h1>Login</h1>
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <form>
+        <input type="email" placeholder="e-mail" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+        <Link to="/register">Create free account</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
   return (
-    <div />
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <form>
+        <input placeholder="Full name" />
+        <input type="email" placeholder="e-mail" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Create account</button>
+        <Link to="/register">I already have a login account</Link>
+      </form>
+    </>
   );
 }

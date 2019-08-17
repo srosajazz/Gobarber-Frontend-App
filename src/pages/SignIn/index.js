3339003421990724a3c3f1003b2a29b22a158636
Sergio Rosa <scarlosjazz@gmail.com>
2019-08-17 08:36:16 -0400

    Yup API schema validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('email is required')
+    .required('email is require'),
+  password: Yup.string().required('Password is require'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -11,7 +19,7 @@ export default function SignIn() {
     <>
       <img src={logo} alt="Gobarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="e-mail" />
         <Input name="password" type="password" placeholder="Password" />
         <button type="submit">Login</button>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is require'),
+  email: Yup.string()
+    .email('email is required')
+    .required('email is require'),
+  password: Yup.string()
+    .min(6, 'Minimum of 6 characters')
+    .required('Password is require'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -11,7 +22,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="Gobarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Full name" />
         <Input name="email" type="email" placeholder="e-mail" />
         <Input name="password" type="password" placeholder="Password" />

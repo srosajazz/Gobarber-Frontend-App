73bec6a75e9d25716e363b6f72d44d82a7e538bc
Sergio Rosa <scarlosjazz@gmail.com>
2019-08-21 18:54:56 -0400

    implenting Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo-purple.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Gobarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Sergio Rosa</strong>
+              <Link to="/profile">My Profile</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Sergio Rosa"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: orange;
+  background: linear-gradient(-90deg, #5c8a8a, #476b6b);
 `;
